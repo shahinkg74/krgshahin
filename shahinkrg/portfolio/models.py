b029dd0fcbc61002d0dc5b4fdf95c526cff00135
shahin <shahinkg70@gmail.com>
2023-11-10 09:40:02 -0800

    templates

--- shahinkrg/portfolio/models.py
+++ shahinkrg/portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Portfolio(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    portfolio = models.ImageField()
+
+    def __str__(self):
+        return self.title
--- shahinkrg/portfolio/views.py
+++ shahinkrg/portfolio/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Portfolio
+
 
 # Create your views here.
+
 def portfolio(request):
-    return render(request, 'portfolio.html')
+    all = Portfolio.objects.all()
+    return render(request, 'portfolio.html', {'all': all})
